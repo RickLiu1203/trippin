000765b012303fee0e5f3bd01f1825ffda846a30
Rick Liu <rickliu1203@gmail.com>
2026-04-03 11:31:18 -0400

    Initial Commit

---
+++ trippin/trippinApp.swift
@@ -0,0 +1,17 @@
+//
+//  trippinApp.swift
+//  trippin
+//
+//  Created by bree on 2026-04-03.
+//
+
+import SwiftUI
+
+@main
+struct trippinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ trippinTests/trippinTests.swift
@@ -0,0 +1,17 @@
+//
+//  trippinTests.swift
+//  trippinTests
+//
+//  Created by bree on 2026-04-03.
+//
+
+import Testing
+@testable import trippin
+
+struct trippinTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
